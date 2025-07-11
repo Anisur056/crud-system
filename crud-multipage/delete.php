17c5bf860f6583c0259f703bddf0eb366191517c
Anisur Rahman <ranisur056@gmail.com>
2025-07-11 20:18:41 +0600

    1.0.5- delete page update

--- crud-multipage/delete.php
+++ crud-multipage/delete.php
@@ -1,15 +1,16 @@
 <?php
+	require ("db-config.php");
+
 	if(isset($_GET['id'])){
 		$id = $_GET['id'];
 	}else{
 		header('location:index.php');
 	}
 
-	$c = mysqli_connect('localhost','root','','test');
-	$do = "DELETE FROM `tbl_user` WHERE id = $id ";
-	$done = mysqli_query($c,$do);
+	$sql = "DELETE FROM `tbl_user` WHERE id = $id ";
+	$execute = mysqli_query($dbConnection,$sql);
 
-	if($done){
+	if($execute){
 		header('location:index.php');
 	}
 
